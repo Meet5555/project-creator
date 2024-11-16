8912fadf1d6818802d590d762e81cb8e39e1ed58
Meet5555 <bhimanimeet55555@gmail.com>
2024-11-16 11:52:31 +0530

    add framework options

--- bin/cli.js
+++ bin/cli.js
@@ -5,40 +5,50 @@ import inquirer from 'inquirer';
 import { generateStructure } from '../generator.js';
 import chalk from 'chalk';
 
+const ALLOWED_FRAMEWORKS = ['react', 'next'];
+
 program
-  .name('create-structure')
+  .name('create-project')
   .description('Generate project structure for React or Next.js applications')
-  .argument('<framework>', 'Framework to generate structure for (react/next)')
+  .argument('[framework]', 'Framework to generate structure for (react/next)')
   .action(async (framework) => {
-    if (!['react', 'next'].includes(framework.toLowerCase())) {
-      console.error(
-        chalk.red('Error: Framework must be either "react" or "next"')
-      );
-      process.exit(1);
-    }
-
-    const answers = await inquirer.prompt([
-      {
-        type: 'confirm',
-        name: 'typescript',
-        message: 'Would you like to use TypeScript?',
-        default: true,
-      },
-      {
-        type: 'confirm',
-        name: 'eslint',
-        message: 'Would you like to include ESLint configuration?',
-        default: true,
-      },
-      {
-        type: 'confirm',
-        name: 'prettier',
-        message: 'Would you like to include Prettier configuration?',
-        default: true,
-      },
-    ]);
-
     try {
+      if (!framework || !ALLOWED_FRAMEWORKS.includes(framework.toLowerCase())) {
+        const frameworkAnswer = await inquirer.prompt([
+          {
+            type: 'list',
+            name: 'framework',
+            message: 'Which framework would you like to use?',
+            choices: ALLOWED_FRAMEWORKS.map((f) => ({
+              name: f.charAt(0).toUpperCase() + f.slice(1),
+              value: f,
+            })),
+          },
+        ]);
+        framework = frameworkAnswer.framework;
+      }
+
+      const answers = await inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'typescript',
+          message: 'Would you like to use TypeScript?',
+          default: true,
+        },
+        {
+          type: 'confirm',
+          name: 'eslint',
+          message: 'Would you like to include ESLint configuration?',
+          default: true,
+        },
+        {
+          type: 'confirm',
+          name: 'prettier',
+          message: 'Would you like to include Prettier configuration?',
+          default: true,
+        },
+      ]);
+
       await generateStructure(framework.toLowerCase(), answers);
     } catch (error) {
       console.error(
